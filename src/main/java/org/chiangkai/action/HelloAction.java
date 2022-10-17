d8e504a36c79f7f07afc8163561c46e53c62a2c4
chiangkai <chiangkk@163.com>
2022-10-17 16:09:26 +0800

    action

--- src/main/java/org/chiangkai/action/HelloAction.java
+++ src/main/java/org/chiangkai/action/HelloAction.java
@@ -1,6 +1,7 @@
 package org.chiangkai.action;
 
 import com.opensymphony.xwork2.Action;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -11,10 +12,22 @@ import org.springframework.stereotype.Controller;
  * @date 2022/10/17
  */
 @Controller
+@Slf4j
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class HelloAction implements Action {
     @Override
     public String execute() throws Exception {
+        log.info("execute");
+        return SUCCESS;
+    }
+
+    public String login() {
+        log.info("login");
+        return SUCCESS;
+    }
+
+    public String register() {
+        log.info("register");
         return SUCCESS;
     }
 }
